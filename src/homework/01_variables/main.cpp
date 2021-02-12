3300156407541b9af49495811336ebff1e629c84
LoganWhite1 <logan.white@g.austincc.edu>
2021-02-11 21:50:03 -0600

    Homework_2

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -7,9 +7,25 @@
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
+include variables.h
 int main()
 {
+	double meal_amount;
+	double tip_rate;
+	double tax_amount;
+	double total;
+
+	cout << "enter the meal amount.\n";
+	cin >> meal_amount;
+
+	tax_amount = meal_amount * get_sales_tax;
 	
+	total_variable = tip_amount + meal_amount;
 
 	return 0;
-}
+	cout:: << "Meal amount:" meal_amount;
+	cout:: << "Sales Tax:" tax_amount;
+	cout:: << "Tip Amount:" tip_amount;
+	cout:: << "total:" total_variable;}
+
+
--- src/homework/01_variables/variables.h
+++ src/homework/01_variables/variables.h
@@ -2,7 +2,21 @@
 int add_numbers(int num1, int num2);
 
 //write function prototype for multiply_numbers with two integer parameters num1, and num2
-
-
+#include <iostream>
+#include <math>
+// Get meal value from user
+int main() {
+    double meal_value;
+    std::cout <<"Enter meal amount";
+    std::cin >> meal_value;
+}
+//create a double value return fucntion named get tip amount
+int main()
+{double tip_rate;
+ double meal_amount;
+ 
+ std::cout <<"enter tip rate.\n";
+ cin:: >> tip_rate;
+  }
 
 
